fb6147751972a2057428b302400a035f63b66e8a
Harshit Raj <harshitraj23092004@gmail.com>
2024-09-28 12:12:03 +0530

    visualization update 4

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(passport.session());
 // Connect to MongoDB
 mongoose.connect("mongodb+srv://Admin-Neural-Brainiacs:Test12345@cluster0.8moz4.mongodb.net/userDBDSA");
 
-
 // Define schema and model for User and Visitor
 const userSchema = new mongoose.Schema({
     username: String,
@@ -53,10 +52,13 @@ passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
-    });
+passport.deserializeUser(async function(id, done) {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
 });
 
 // Routes
